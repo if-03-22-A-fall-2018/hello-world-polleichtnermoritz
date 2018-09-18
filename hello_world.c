98d3569b7fc258cedf38d5abc6eab367aebe5cb3
polleichtnermoritz <moritz.polleichtner@outlook.com>
2018-09-18 09:48:11 +0200

    first version

---
+++ hello_world.c
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			hello world
+ * Author:			Moritz Polleichtne
+ * ----------------------------------------------------------
+ * Description:
+ * simple hello world program
+ * ----------------------------------------------------------
+ */
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+    printf("Hello Moritz\n");
+  return 0;
+}
