37267d217f8166e6b210526ef0de330b5919b3ce
polleichtnermoritz <43366689+polleichtnermoritz@users.noreply.github.com>
2018-09-21 18:02:51 +0200

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:			Hello World
+ * Author:			Moritz Polleichtner
+ * ----------------------------------------------------------
+ * Description:
+ * simple "hello world" print out in the terminal
+ * ----------------------------------------------------------
+ */
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("hello Moritz Polleichtner\n");
+  return 0;
+}
